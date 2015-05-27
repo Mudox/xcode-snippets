2a77f422dcaace575e3df036d7249c1ebb5050e8
Mudox <mudoxisme@gmail.com>
2015-05-27 17:03:03 +0800

    add prefix to add_vfl_based_constraints.

--- add_vfl_based_constraints.m
+++ add_vfl_based_constraints.m
@@ -1,6 +1,7 @@
 // Add VFL based constraints
-// 
 //
+//
+// IDECodeSnippetCompletionPrefix: acs
 // IDECodeSnippetCompletionScopes: [All]
 // IDECodeSnippetIdentifier: A8EC25A6-5F8B-4576-9AE5-A4977DC48F2A
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
